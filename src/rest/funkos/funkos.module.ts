0fbae9a81bfcfcecdd141c44edf063bc93dc9e05
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-18 08:13:55 +0100

    feat(module): Añadimos mapper al modulo

--- src/rest/funkos/funkos.module.ts
+++ src/rest/funkos/funkos.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { FunkosService } from './funkos.service'
 import { FunkosController } from './funkos.controller'
+import { FunkoMapper } from './mappers/funko-mapper'
 
 @Module({
   controllers: [FunkosController],
-  providers: [FunkosService],
+  providers: [FunkosService, FunkoMapper],
 })
 export class FunkosModule {}
