26657d8f421f5f71a6cec626f6476973b3666171
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-18 10:29:55 +0100

    feat(typeOrm): Añadimos typeOrm al proyecto

--- src/rest/categories/categories.module.ts
+++ src/rest/categories/categories.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common'
 import { CategoriesService } from './categories.service'
 import { CategoriesController } from './categories.controller'
-import { CategoryMapper } from './mappers/category-mapper';
+import { CategoryMapper } from './mappers/category-mapper'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Category } from './entities/category.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoryMapper],
 })
