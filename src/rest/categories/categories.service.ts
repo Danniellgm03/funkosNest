78f69033024f5c781eb32b433451ab43a3cec047
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-18 10:30:41 +0100

    feat(service): Añadimos en el servicio consultas con typeOrm

--- src/rest/categories/categories.service.ts
+++ src/rest/categories/categories.service.ts
@@ -8,32 +8,33 @@ import { CreateCategoryDto } from './dto/create-category.dto'
 import { UpdateCategoryDto } from './dto/update-category.dto'
 import { Category } from './entities/category.entity'
 import { CategoryMapper } from './mappers/category-mapper'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
 
 @Injectable()
 export class CategoriesService {
-  private categories: Category[] = []
-  private idCount = 1
   private readonly logger = new Logger(CategoriesService.name)
 
-  constructor(private readonly CategoryMapper: CategoryMapper) {}
+  constructor(
+    private readonly CategoryMapper: CategoryMapper,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+  ) {}
 
-  create(createCategoryDto: CreateCategoryDto) {
-    const category = this.CategoryMapper.toEntity(
-      createCategoryDto,
-      this.idCount++,
-    )
-    this.categories.push(category)
+  async create(createCategoryDto: CreateCategoryDto) {
+    const category = this.CategoryMapper.toEntity(createCategoryDto)
+    await this.categoryRepository.save(category)
     this.logger.log(`La categoría ${category.name} ha sido creada`)
     return category
   }
 
-  findAll() {
+  async findAll() {
     this.logger.log(`Buscando todas las categorias`)
-    return this.categories
+    return await this.categoryRepository.find()
   }
 
-  findById(id: number) {
-    const category = this.categories.find((c) => c.id == id)
+  async findById(id: number) {
+    const category = await this.categoryRepository.findOneBy({ id })
     this.logger.log(`Buscando categoria con el id: ${id}`)
     if (!category) {
       throw new NotFoundException(`La categoria con el id: ${id} no existe`)
@@ -41,8 +42,8 @@ export class CategoriesService {
     return category
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    const category = this.findById(id)
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.findById(id)
 
     if (
       updateCategoryDto.name != undefined ||
@@ -51,6 +52,7 @@ export class CategoriesService {
       this.logger.log(`Actualizando categoria con id ${id}`)
       category.name = updateCategoryDto.name ?? category.name
       category.active = updateCategoryDto.active ?? category.active
+      await this.categoryRepository.save(category)
     } else {
       throw new BadRequestException(
         'Para actualizar una categoria es necesario alguno de los siguientes datos (name, active)',
@@ -60,12 +62,11 @@ export class CategoriesService {
     return category
   }
 
-  remove(id: number) {
-    const categoryIndex = this.categories.findIndex((c) => c.id == id)
-    if (categoryIndex > -1) {
-      this.logger.log(`Borrando categoria con id ${id}`)
-      this.categories.splice(categoryIndex, 1)
-      return
+  async remove(id: number) {
+    const category = await this.findById(id)
+
+    if (category) {
+      await this.categoryRepository.delete(category.id)
     } else {
       throw new NotFoundException(`La categoria con el id: ${id} no existe`)
     }
