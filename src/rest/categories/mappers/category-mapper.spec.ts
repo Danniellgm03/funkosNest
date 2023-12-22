3c46e1070a603e7f5a87e5494b322b422c3f45d5
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-23 00:08:45 +0100

    style(mapper): Quitamos puntos y comas

--- src/rest/categories/mappers/category-mapper.spec.ts
+++ src/rest/categories/mappers/category-mapper.spec.ts
@@ -1,18 +1,18 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { CategoryMapper } from './category-mapper';
+import { Test, TestingModule } from '@nestjs/testing'
+import { CategoryMapper } from './category-mapper'
 
 describe('CategoryMapper', () => {
-  let provider: CategoryMapper;
+  let provider: CategoryMapper
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [CategoryMapper],
-    }).compile();
+    }).compile()
 
-    provider = module.get<CategoryMapper>(CategoryMapper);
-  });
+    provider = module.get<CategoryMapper>(CategoryMapper)
+  })
 
   it('should be defined', () => {
-    expect(provider).toBeDefined();
-  });
-});
+    expect(provider).toBeDefined()
+  })
+})
