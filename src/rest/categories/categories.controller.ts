dbdd00d7aec9bfe0746c46c64eee6da6c97e7903
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-23 00:05:58 +0100

    feat(controller): Agregamos validaciones de categoria

--- src/rest/categories/categories.controller.ts
+++ src/rest/categories/categories.controller.ts
@@ -5,13 +5,15 @@ import {
   Body,
   Param,
   Delete,
-  ParseIntPipe,
   HttpCode,
   Put,
+  ParseUUIDPipe,
+  UsePipes,
 } from '@nestjs/common'
 import { CategoriesService } from './categories.service'
 import { CreateCategoryDto } from './dto/create-category.dto'
 import { UpdateCategoryDto } from './dto/update-category.dto'
+import { NoWithspacesPipe } from '../../pipes/no-withspaces.pipe'
 
 @Controller('categories')
 export class CategoriesController {
@@ -23,26 +25,28 @@ export class CategoriesController {
   }
 
   @Get(':id')
-  async findOne(@Param('id', ParseIntPipe) id: number) {
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
     return await this.categoriesService.findById(id)
   }
 
   @Post()
   @HttpCode(201)
+  @UsePipes(new NoWithspacesPipe({ fields: ['name'] }))
   async create(@Body() createCategoryDto: CreateCategoryDto) {
     return await this.categoriesService.create(createCategoryDto)
   }
 
   @Put(':id')
+  @UsePipes(new NoWithspacesPipe({ fields: ['name'] }))
   async update(
-    @Param('id', ParseIntPipe) id: number,
+    @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCategoryDto: UpdateCategoryDto,
   ) {
     return await this.categoriesService.update(id, updateCategoryDto)
   }
 
   @Delete(':id')
-  async remove(@Param('id', ParseIntPipe) id: number) {
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
     return await this.categoriesService.remove(id)
   }
 }
