026c399e55d15f1750bf47ccb776a576a167bcbb
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-23 00:10:21 +0100

    feat(controller):Agregamos validaciones y asincronia funkos

--- src/rest/funkos/funkos.controller.ts
+++ src/rest/funkos/funkos.controller.ts
@@ -8,43 +8,46 @@ import {
   HttpCode,
   Put,
   ParseIntPipe,
+  UsePipes,
 } from '@nestjs/common'
 import { FunkosService } from './funkos.service'
 import { CreateFunkoDto } from './dto/create-funko.dto'
 import { UpdateFunkoDto } from './dto/update-funko.dto'
+import { NoWithspacesPipe } from '../../pipes/no-withspaces.pipe'
 
 @Controller('funkos')
 export class FunkosController {
   constructor(private readonly funkosService: FunkosService) {}
 
   @Get()
-  findAll() {
-    return this.funkosService.findAll()
+  async findAll() {
+    return await this.funkosService.findAll()
   }
 
   @Get(':id')
-  findById(@Param('id', ParseIntPipe) id: number) {
-    return this.funkosService.findById(id)
+  async findById(@Param('id', ParseIntPipe) id: number) {
+    return await this.funkosService.findById(id)
   }
 
   @Post()
   @HttpCode(201)
-  create(@Body() createFunkoDto: CreateFunkoDto) {
-    return this.funkosService.create(createFunkoDto)
+  @UsePipes(new NoWithspacesPipe({ fields: ['name', 'image', 'category'] }))
+  async create(@Body() createFunkoDto: CreateFunkoDto) {
+    return await this.funkosService.create(createFunkoDto)
   }
 
   @Put(':id')
-  update(
+  @UsePipes(new NoWithspacesPipe({ fields: ['name', 'image', 'category'] }))
+  async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateFunkoDto: UpdateFunkoDto,
   ) {
-    console.log(updateFunkoDto)
-    return this.funkosService.update(id, updateFunkoDto)
+    return await this.funkosService.update(id, updateFunkoDto)
   }
 
   @Delete(':id')
   @HttpCode(204)
-  remove(@Param('id', ParseIntPipe) id: number) {
-    this.funkosService.remove(id)
+  async remove(@Param('id', ParseIntPipe) id: number) {
+    await this.funkosService.remove(id)
   }
 }
