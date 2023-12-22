82a91b382344c7ba0347d34da0c2fb9e60e1ada6
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-23 00:06:22 +0100

    feat(module): Agregamos repositorio al modulo de categoria

--- src/rest/categories/categories.module.ts
+++ src/rest/categories/categories.module.ts
@@ -4,9 +4,13 @@ import { CategoriesController } from './categories.controller'
 import { CategoryMapper } from './mappers/category-mapper'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Category } from './entities/category.entity'
+import { Funko } from '../funkos/entities/funko.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [
+    TypeOrmModule.forFeature([Category]),
+    TypeOrmModule.forFeature([Funko]),
+  ],
   controllers: [CategoriesController],
   providers: [CategoriesService, CategoryMapper],
 })
